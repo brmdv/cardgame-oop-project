7bb7925b13465cf89b928f4c2a0085ef3b255f03
Bram De Vroey <bramdevroey@gmail.com>
2021-01-15 09:38:54 +0100

    Revert "Added __repr__ for Card for debugging purposes."
    This reverts commit b558e99229b2abac00eafa084186f1b24bdf61c0.

--- utils/card.py
+++ utils/card.py
@@ -63,6 +63,3 @@ class Card(Symbol):
 
     def __str__(self) -> str:
         return super().__str__() + self.value
-
-    def __repr__(self) -> str:
-        return f"<Card {super().__str__()}{self.value} {id(self)}>"
